9bccacd3cb229edfaec76289e65ba80cfaf6b54f
Pavel <sx000@1-tn.com>
2025-03-05 15:17:10 +0100

    3 test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CapRover2!"
+    return "Hello from CapRover3!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
